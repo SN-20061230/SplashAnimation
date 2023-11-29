171362d09745524d7882fd976a3d1f85d54f9b4f
Handy2 <aqayumov96@gmail.com>
2023-11-29 11:13:00 +0500

    Hala Madrid

--- app/src/main/java/com/example/splashanimation/SplashScreen.kt
+++ app/src/main/java/com/example/splashanimation/SplashScreen.kt
@@ -1,6 +1,9 @@
 package com.example.splashanimation
 
 
+import android.view.animation.OvershootInterpolator
+import androidx.compose.animation.Animatable
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
@@ -9,23 +12,34 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.res.painterResource
 import androidx.navigation.NavController
+import com.example.splashanimation.Navigation.Screens
 import kotlinx.coroutines.delay
 
 @Composable
 fun SplashScreen(navController: NavController) {
-    LaunchedEffect(key1 = true) {
-        delay(2000)
-        navController.navigate("main_screen")
+    val scale = remember {
+        androidx.compose.animation.core.Animatable(0f)
     }
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center,
-    ) {
-        Image(
-            painter = painterResource(R.drawable.logo),
-            contentDescription = "Splash",
+    LaunchedEffect(key1 = true) {
+        scale.animateTo(
+            targetValue =1.5f,
+            animationSpec = tween(
+                durationMillis = 800,
+                easing = {
+                    OvershootInterpolator(1.7f).getInterpolation(it)
+                })
         )
+        delay(3000L)
+        navController.navigate(Screens.HomeScreen.route)
+    }
+    // Image
+    Box(contentAlignment = Alignment.Center,
+        modifier = Modifier.fillMaxSize()) {
+        Image(painter = painterResource(id = R.drawable.logo),
+            contentDescription = "Logo",
+            modifier = Modifier.scale(scale.value))
     }
 }
